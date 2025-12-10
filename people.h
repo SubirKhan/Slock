2337e5abd9e297c6e900b601995d87962d2afe79
SubirKhan <subir.khan2020@gmail.com>
2025-12-09 18:23:03 -0800

    making classes

---
+++ people.h
@@ -0,0 +1,17 @@
+class User {
+public:
+
+private: 
+
+};
+
+ 
+class Landlord : public User { // Landlord class is inherting from USer (parent) 
+
+};
+
+
+class Guest : public User {  // Guest class is inherting from USer (parent)
+
+
+};
